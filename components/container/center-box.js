252b6153bce46d6cb1acbf023a7047008540afdf
neuronishad <149999734+neuronishad@users.noreply.github.com>
2025-07-10 16:12:34 +0530

    Update center-box.js

--- components/container/center-box.js
+++ components/container/center-box.js
@@ -3,9 +3,22 @@ import { EUIContainerElement } from './eui-container.js';
 export class EUICenterBox extends EUIContainerElement {
   constructor() {
     super('column');
+  }
+
+  connectedCallback() {
     this.style.height = '100%';
     this.style.width = '100%';
     this.style.boxSizing = 'border-box';
+
+    // Default justify-content and align-items if not set
+    if (!this.hasAttribute('justify-content')) {
+      this.setAttribute('justify-content', 'center');
+    }
+    if (!this.hasAttribute('align-items')) {
+      this.setAttribute('align-items', 'center');
+    }
+
+    super.connectedCallback(); // call to _applyStyles via base class
   }
 
   get justifyContent() {
